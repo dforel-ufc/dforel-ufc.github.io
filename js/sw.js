c10cf23b90e83415d469bf6ec735050648a99a71
dforel <duncan.forel@gmail.com>
2019-11-19 11:46:53 +0100

    Correction sw

--- js/sw.js
+++ js/sw.js
@@ -24,8 +24,8 @@ var appShellFiles = [
 ];
 
 self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
   e.waitUntil(
-    console.log('[Service Worker] Install');
     caches.open(nameOfCache).then((cache) => {
         console.log('[Service Worker] Caching all: app shell and content');
         return cache.addAll(appShellFiles);
